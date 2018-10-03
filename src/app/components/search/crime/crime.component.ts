e35b8861692a2aef04846e7c8ebc6e8b750155cf
Nathan Kemppainen <nathan.kemppainen@gmail.com>
2018-10-02 22:48:15 -0700

    Refactored crimes and connected it to the mainSearchGroup form

--- src/app/components/search/crime/crime.component.ts
+++ src/app/components/search/crime/crime.component.ts
@@ -1,17 +1,28 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, ControlContainer } from '@angular/forms';
 
 @Component({
   selector: 'crime-search-component',
   templateUrl: './crime.component.html',
   styleUrls: ['./crime.component.scss']
 })
+
 export class CrimeComponent implements OnInit {
 	@Input() crime: FormGroup;
 
-  constructor() { }
+	crimesGroup = [
+	{display:'Crime', 			 name:'criminalAct', placeholder:'Murder, Blackmail'},
+	{display:'Perpetrator', name:'perpetrator', placeholder:'(Name)'},
+	{display:'Victim',			 name:'victim', 		 placeholder:'(Name)'},
+	{display:'Means', 			name:'means',  			placeholder:'Pistol, Poison, Knife'},
+	{display:'Motive', 			name:'motive', 			placeholder:'Greed,Jealousy'},
+	{display:'Opportunity', name:'opportunity', placeholder:'Posing as, Following'}
+	];
+
+  constructor(private controlContainer: ControlContainer) { }
 
   ngOnInit() {
+  	this.crime = <FormGroup>this.controlContainer.control;
   }
 
 }
