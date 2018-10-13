8758d38e1e9272851c9e9cd3ebd5ea52430c77bc
Nathan Kemppainen <nathan.kemppainen@gmail.com>
2018-10-12 23:14:51 -0700

    Routing updated to include /search

--- src/app/components/routing/routes.ts
+++ src/app/components/routing/routes.ts
@@ -3,10 +3,13 @@ import { Routes } from '@angular/router';
 import { HowtouseComponent } from '../howtouse/howtouse.component';
 import { SearchComponent   } from '../search/search.component';
 import { ResultsComponent  } from '../results/results.component';
+import { EpisodeDetailComponent } from '../episode-detail/episode-detail.component';
 
 export const routes: Routes = [
   { path: 'howtouse',  component: HowtouseComponent },
   { path: 'howtouse',  component: HowtouseComponent },  
   { path: 'home',      component: ResultsComponent },
+  { path: 'search',    component: SearchComponent },
+  { path: 'id/:episodeId',      component: EpisodeDetailComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
